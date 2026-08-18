27ccaac310b497c8a96dc0a4560fd1397ffa88a9
MONISHA C <monishaacharc@gmail.com>
2026-08-19 01:34:57 +0530

    ReactPortfolio_changes

---
+++ src/components/AboutSection/AboutSection.jsx
@@ -0,0 +1,19 @@
+import './AboutSection.css';
+
+function AboutSection() {
+  return (
+    <section id='about' className='about-section'>
+      <div className='about-wrapper container'>
+        <div className='about-image'>
+          <img src='public/profile.jpeg' alt='Monisha C' />
+        </div>
+        <div className='about-copy'>
+          <h1>About Me</h1>
+          <p>Hi! I'm Monisha C, a developer based in India building fast, accessible web experiences.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutSection;
