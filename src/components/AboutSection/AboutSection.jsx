584612e8bde46bcccf3c23da6f68ed0f77017b4b
MONISHA C <monishaacharc@gmail.com>
2026-08-23 01:32:34 +0530

    day14

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection() {
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
         <div className='about-image'>
-          <img src='public/profile.jpeg' alt='Monisha C' />
+          <img src='/profile.jpeg' alt='Monisha C' />
         </div>
         <div className='about-copy'>
           <h1>About Me</h1>
